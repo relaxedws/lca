94878f1f5e3550fa32aabf1a88ff4886187ab11b
Rakesh Verma <rkv160794@gmail.com>
2016-06-09 19:45:24 +0530

    Creating vertices with generateVertices Method now.

--- src/LowestCommonAncestor.php
+++ src/LowestCommonAncestor.php
@@ -6,31 +6,36 @@ use Fhaculty\Graph\Vertex;
 use Fhaculty\Graph\Graph;
 use Graphp\Algorithms\Search\BreadthFirst;
 
-class LowestCommonAncestor {
-  
-  function __construct(Graph $graph) {
-    $this->graph = $graph;
-  }
-
-  function find(Vertex $local, Vertex $remote) {
-    // Traverse in reverse order the graph when searching;
-    $direction = BreadthFirst::DIRECTION_REVERSE;
-    // Use BFS algorithm to get all vertices starting with $local to the root.
-    $bfs_local = new BreadthFirst($local);
-    $vertices_local = $bfs_local->setDirection($direction)->getVertices()->getMap();
-    // Use BFS algorithm to get all vertices starting with $remote to the root.
-    $bfs_remote = new BreadthFirst($remote);
-    $vertices_remote = $bfs_remote->setDirection($direction)->getVertices()->getMap();
-    // Intersect the $vertices_local and $vertices_remote to get all common
-    // ancestors for $local and $remote.
-    $vertices_intersection = array_intersect(array_keys($vertices_local), array_keys($vertices_remote));
-    // If the $vertices_intersection array is not empty then first value should
-    // be our solution - the LCA.
-    $lca_id = reset($vertices_intersection);
-    if ($lca_id !== NULL && $lca = $this->graph->getVertex($lca_id)) {
-      return $lca;
+class LowestCommonAncestor
+{
+    public function __construct(Graph $graph)
+    {
+        $this->graph = $graph;
     }
-    return FALSE;
-  }
+
+    /**
+    * @param Vertex $local
+    * @param Vertex $remote
+    * @return bool
+    */
+    public function find(Vertex $local, Vertex $remote)
+    {
+        // Traverse in reverse order the graph when searching;
+        $direction = BreadthFirst::DIRECTION_REVERSE;
+        // Use BFS algorithm to get all vertices starting with $local to the root.
+        $bfs_local = new BreadthFirst($local);
+        $vertices_local = $bfs_local->setDirection($direction)->getVertices();
+        // Use BFS algorithm to get all vertices starting with $remote to the root.
+        $bfs_remote = new BreadthFirst($remote);
+        $vertices_remote = $bfs_remote->setDirection($direction)->getVertices();
+        // Intersect the $vertices_local and $vertices_remote to get all common
+        // ancestors for $local and $remote. If the $vertices_intersection array is
+        // not empty then first value should be our solution - the LCA.
+        $vertices_intersection = $vertices_local
+            ->getVerticesIntersection($vertices_remote)
+            ->getMap();
+        return reset($vertices_intersection) ?: FALSE;
+    }
 
 }
+
--- tests/LowestCommonAncestorTest.php
+++ tests/LowestCommonAncestorTest.php
@@ -4,71 +4,63 @@ namespace Relaxed\LCA\Test;
 
 use Fhaculty\Graph\Graph;
 use Relaxed\LCA\LowestCommonAncestor;
-use Fhaculty\Graph\Vertex;
-use Graphp\Algorithms\Search\BreadthFirst;
 
 class LowestCommonAncestorTest extends \PHPUnit_Framework_TestCase
 {
-    public $graph;
-    public function testlca()
+    public function testLca()
     {
         $graph = new Graph();
-
-        $one = $graph->createVertex('One');
-        $two = $graph->createVertex('Two');
-        $three = $graph->createVertex('Three');
-        $four = $graph->createVertex('Four');
-        $five =$graph->createVertex('Five');
-        $six = $graph->createVertex('Six');
-        $seven = $graph->createVertex('Seven');
-        $eight = $graph->createVertex('Eight');
-        $nine = $graph->createVertex('Nine');
-        $ten = $graph->createVertex('Ten');
-        $eleven = $graph->createVertex('Eleven');
-        $twelve = $graph->createVertex('Twelve');
-        $thirteen =$graph->createVertex('Thirteen');
-        $fourteen = $graph->createVertex('Fourteen');
-        $fifteen = $graph->createVertex('Fifteen');
-        $sixteen = $graph->createVertex('Sixteen');
-        $seventeen= $graph->createVertex('Seventeen');
-        $eighteen = $graph->createVertex('Eighteen');
-        $nineteen = $graph->createVertex('Nineteen');
-        $twenty = $graph->createVertex('Twenty');
-        $twenty_one = $graph->createVertex('Twenty One');
-
-        $one->createEdgeTo($three);
-        $one->createEdgeTo($two);
-        $two->createEdgeTo($four);
-        $three->createEdgeTo($eight);
-        $four->createEdgeTo($five);
-        $four->createEdgeTo($six);
-        $four->createEdgeTo($seven);
-        $eight->createEdgeTo($nine);
-        $eight->createEdgeTo($ten);
-        $five->createEdgeTo($eleven);
-        $five->createEdgeTo($twelve);
-        $twelve->createEdgeTo($fourteen);
-        $twelve->createEdgeTo($fifteen);
-        $six->createEdgeTo($thirteen);
-        $seven->createEdgeTo($sixteen);
-        $nine->createEdgeTo($seventeen);
-        $seventeen->createEdgeTo($twenty);
-        $seventeen->createEdgeTo($nineteen);
-        $ten->createEdgeTo($eighteen);
-        $eighteen->createEdgeTo($twenty_one);
+        //Creating new Graph with 21 nodes.
+        $vertices = $this->generateVertices($graph,21);
+        //Creating edges starting from root(node_1) to node_21
+        $vertices['node_1']->createEdgeTo($vertices['node_2']);
+        $vertices['node_1']->createEdgeTo($vertices['node_3']);
+        $vertices['node_3']->createEdgeTo($vertices['node_8']);
+        $vertices['node_2']->createEdgeTo($vertices['node_4']);
+        $vertices['node_4']->createEdgeTo($vertices['node_5']);
+        $vertices['node_4']->createEdgeTo($vertices['node_6']);
+        $vertices['node_4']->createEdgeTo($vertices['node_7']);
+        $vertices['node_5']->createEdgeTo($vertices['node_11']);
+        $vertices['node_5']->createEdgeTo($vertices['node_12']);
+        $vertices['node_6']->createEdgeTo($vertices['node_13']);
+        $vertices['node_7']->createEdgeTo($vertices['node_16']);
+        $vertices['node_8']->createEdgeTo($vertices['node_9']);
+        $vertices['node_8']->createEdgeTo($vertices['node_10']);
+        $vertices['node_9']->createEdgeTo($vertices['node_17']);
+        $vertices['node_10']->createEdgeTo($vertices['node_18']);
+        $vertices['node_12']->createEdgeTo($vertices['node_14']);
+        $vertices['node_12']->createEdgeTo($vertices['node_15']);
+        $vertices['node_17']->createEdgeTo($vertices['node_19']);
+        $vertices['node_17']->createEdgeTo($vertices['node_20']);
+        $vertices['node_18']->createEdgeTo($vertices['node_21']);
 
         $lca = new LowestCommonAncestor($graph);
 
         //Test cases for different pair of nodes.   
-        $test_node1 = $lca->find($thirteen, $fourteen);
-        $this->assertEquals('Four', $test_node1->getId());
-        $test_node2 = $lca->find($eleven, $fifteen);
-        $this->assertEquals('Five', $test_node2->getId());
-        $test_node3 = $lca->find($sixteen, $seventeen);
-        $this->assertEquals('One', $test_node3->getId());
-        $test_node4 = $lca->find($nine, $seventeen);
-        $this->assertEquals('Nine', $test_node4->getId());
-        $test_node5 = $lca->find($twenty, $nineteen);
-        $this->assertEquals('Seventeen', $test_node5->getId());
+        $test_node1 = $lca->find($vertices['node_14'], $vertices['node_15']);
+        $this->assertEquals('node_12', $test_node1->getId());
+        $test_node2 = $lca->find($vertices['node_11'], $vertices['node_15']);
+        $this->assertEquals('node_5', $test_node2->getId());
+        $test_node3 = $lca->find($vertices['node_16'], $vertices['node_17']);
+        $this->assertEquals('node_1', $test_node3->getId());
+        $test_node4 = $lca->find($vertices['node_9'], $vertices['node_17']);
+        $this->assertEquals('node_9', $test_node4->getId());
+        $test_node5 = $lca->find($vertices['node_20'], $vertices['node_19']);
+        $this->assertEquals('node_17', $test_node5->getId());
+    }
+
+    /**
+     * Function to create multiple vertices.
+     * @param Graph $graph
+     * @param int $count
+     * @return \Fhaculty\Graph\Vertex[]
+     */
+    public function generateVertices(Graph $graph, $count = 5)
+    {
+        for ($i = 1; $i <= $count; $i++)
+        {
+            $ids[] = "node_$i";
+        }
+        return $graph->createVertices($ids)->getMap();
     }
 }
