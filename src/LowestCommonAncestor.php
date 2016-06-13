80d64bd0af5a1790f2250dbc1857ac111fd4a3ce
Rakesh Verma <rkv160794@gmail.com>
2016-06-13 19:32:34 +0530

    Implement tests for LCA library. (#2)
    * Writing tests for LCA class
    
    * Writing test cases for LCA
    
    * Basic test case working fine
    
    * Basic test case working fine
    
    * Added few more test cases for simple graph
    
    * Added few more test cases for simple graph
    
    * Modified test cases.
    
    * Creating vertices with generateVertices Method now.
    
    * added pictures folders and implement more test cases with complex graphs
    
    * Comments added in test file
    
    * Comments added in test file
    
    * Added exception for no parent, tests for same node in remote and local and for loop over node
    
    * Tested with DIRECTION_FORWARD,updated test cases.
    
    * removed constructor, lca and unused name referenses.
    
    * Correct syntax mistake.
    
    * Correct syntax mistake.
    
    * fixed the Exception handler using 'assertTrue' method and removed the direction parameter from find() method.
    
    * fixed the Exception handler using 'assertTrue' method and removed the direction parameter from find() method.
    
    * updated comment
    
    * Followed PSR-2 standard and updated description of find function
    
    * Added description before parameters

--- src/LowestCommonAncestor.php
+++ src/LowestCommonAncestor.php
@@ -3,34 +3,46 @@
 namespace Relaxed\LCA;
 
 use Fhaculty\Graph\Vertex;
-use Fhaculty\Graph\Graph;
 use Graphp\Algorithms\Search\BreadthFirst;
+use Fhaculty\Graph\Exception\UnderflowException;
 
-class LowestCommonAncestor {
-
-  function __construct(Graph $graph) {
-    $this->graph = $graph;
-  }
-
-  function find(Vertex $local, Vertex $remote) {
-    // Traverse in reverse order the graph when searching;
-    $direction = BreadthFirst::DIRECTION_REVERSE;
-    // Use BFS algorithm to get all vertices starting with $local to the root.
-    $bfs_local = new BreadthFirst($local);
-    $vertices_local = $bfs_local->setDirection($direction)->getVertices()->getMap();
-    // Use BFS algorithm to get all vertices starting with $remote to the root.
-    $bfs_remote = new BreadthFirst($remote);
-    $vertices_remote = $bfs_remote->setDirection($direction)->getVertices()->getMap();
-    // Intersect the $vertices_local and $vertices_remote to get all common
-    // ancestors for $local and $remote.
-    $vertices_intersection = array_intersect(array_keys($vertices_local), array_keys($vertices_remote));
-    // If the $vertices_intersection array is not empty then first value should
-    // be our solution - the LCA.
-    $lca_id = reset($vertices_intersection);
-    if ($lca_id !== NULL && $lca = $this->graph->getVertex($lca_id)) {
-      return $lca;
+class LowestCommonAncestor
+{
+    /**
+     * The function finds out the lowest common ancestor(LCA) of two nodes.
+     *              1
+     *            /  \
+     *           2    5
+     *         /  \    \
+     *        3    4    6
+     *  For example: LCA of (3,4) would be 2
+     *  LCA of (3,6) would be 1.
+     * @param Vertex $local
+     * @param Vertex $remote
+     * @return Lowest common ancestor from the graph.
+     */
+    public function find(Vertex $local, Vertex $remote)
+    {
+        // Traverse in reverse order starting from $local to the root.
+        $direction = BreadthFirst::DIRECTION_REVERSE;
+        // Use BFS algorithm to get all vertices starting with $local to the root.
+        $bfs_local = new BreadthFirst($local);
+        $vertices_local = $bfs_local->setDirection($direction)->getVertices();
+        // Use BFS algorithm to get all vertices starting with $remote to the root.
+        $bfs_remote = new BreadthFirst($remote);
+        $vertices_remote = $bfs_remote->setDirection($direction)->getVertices();
+        // Intersect the $vertices_local and $vertices_remote to get all common
+        // ancestors for $local and $remote. If the $vertices_intersection array is
+        // not empty then first value should be our solution - the LCA.
+        $vertices_intersection = $vertices_local
+            ->getVerticesIntersection($vertices_remote)
+            ->getMap();
+        if ($lca = reset($vertices_intersection)) {
+            return  $lca;
+        }
+        else {
+            throw new UnderflowException("No common ancestor found");
+        }
     }
-    return FALSE;
-  }
-
 }
+
